9b9ba3dbdf68d6c3b59dcdaed0f5788aaa3a8ae2
Movlar <budik.v@yandex.ru>
2024-09-23 00:17:42 +0300

    commit

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_origins=[
         # "http://localhost:5173",
         # "http://31.129.43.117",
-        "http://45.12.19.6",
+        "http://movlar-dev.ru",
     ],
     allow_credentials=True,
     allow_methods=["*"],
